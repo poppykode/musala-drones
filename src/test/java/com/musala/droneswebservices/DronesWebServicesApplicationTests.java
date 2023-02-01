e268c076927db6a0b3ab440eaac16a2b14c62ac1
poppykode <ngonimug@gmail.com>
2023-02-01 12:19:23 +0200

    initial project set up of common packages & database

---
+++ src/test/java/com/musala/droneswebservices/DronesWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.droneswebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
